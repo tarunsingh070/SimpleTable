ee69ed24f8b7ce0580ea26b6c4a1d36d79f96282
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2017-11-22 19:04:51 -0500

    Initial Commit

---
+++ Simple Table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Table
+//
+//  Created by Tarun Singh on 2017-11-22.
+//  Copyright © 2017 Tarun Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
