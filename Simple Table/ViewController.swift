59213c5815b841389de56f7ea4a00158e52831db
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2017-11-22 19:48:57 -0500

    Showed data in table view.

--- Simple Table/ViewController.swift
+++ Simple Table/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let movies = ["Batman vs Superman", "Blade runner 2049", "Thor: Ragnarak", "Lord of the Rings"
+    , "Wonder Woman", "Pursuit of Happyness"]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        cell?.textLabel?.text = movies[indexPath.row]
+        return cell!
+    }
 
 }
 
